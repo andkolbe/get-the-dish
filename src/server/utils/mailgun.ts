1a28ac10a49860e73af05a981771ba076f087b08
Andrew <kolbe1129@gmail.com>
2021-04-20 11:31:48 -0500

    Updated Mailgun

--- src/server/utils/mailgun.ts
+++ src/server/utils/mailgun.ts
@@ -1,9 +1,14 @@
-import config from '../config';
 //@ts-ignore
 import * as inlineCSS from 'inline-css';
-import * as mailgunLoader from 'mailgun-js';
+import * as FormData from 'form-data';
+import * as Mailgun from 'mailgun.js';
+import MailGun from 'mailgun.js';
+import config from '../config';
 
-const mailgun = mailgunLoader({ apiKey: config.keys.mailgun, domain: config.keys.mailgun_domain })
+const mailgun = new (<typeof MailGun>(<any>Mailgun))(<any>FormData).client({
+    username: 'api',
+    key: config.keys.mailgun
+})
 
 const template = `
 <div>
@@ -14,20 +19,20 @@ const template = `
 
     <h1>Welcome to Get the Dish!</h1>
 
-    <h2>Thank you for signing up to Get the Dish! Here, people can together and post about their favorite dishes.</h2>
+    <h2>Thank you for signing up to Get the Dish! Here, people can come together and post about their favorite dishes.</h2>
     <h3>I only started coding a few months ago, so any 
     feedback on the design or functionality would be greatly appreciated. Just drop it in the Contact form. Have fun!</h3>
 <div>
 `;
 
 export const welcomeEmail = async (to: string, from: string) => {
-    const html = await inlineCSS(template, { url: 'fake' }); // eventually change to get the dish url
+    const html = await inlineCSS(template, { url: 'https://get-the-dish.herokuapp.com/' }); 
     const data = {
         to, 
         from,
         html
     }
-    return mailgun.messages().send(data);
+    return mailgun.messages.create(config.keys.mailgun_domain, data);
 }
 
 export const contactEmail = async (to: string, from: string, subject: string, content: string) => {
@@ -37,6 +42,6 @@ export const contactEmail = async (to: string, from: string, subject: string, co
         subject,
         text: content
     }
-    return mailgun.messages().send(data);
+    return mailgun.messages.create(config.keys.mailgun_domain, data);
 }
 
